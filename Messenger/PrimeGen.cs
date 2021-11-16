93b318c4e4538f89c22d588a00befef518c77c07
Daniel Kim <dkk6651@rit.edu>
2021-11-15 23:55:29 -0500

    Partial implementation of KeyGen and getKey Added ModInverse and PrimeGen related methods for keyGen Purposes

--- Messenger/PrimeGen.cs
+++ Messenger/PrimeGen.cs
@@ -1,93 +1,10 @@
-﻿/// Name: Daniel Kim
-///
-/// 
-
-using System;
-using System.IO;
-using System.Net.Http;
 using System.Numerics;
 using System.Security.Cryptography;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Messenger
 {
-    class EncryptedFile
+    public class PrimeGen
     {
-        public String email { get; set; }
-        public String key { get; set; }
-    }
-
-    class Program
-    {
-        private static readonly HttpClient client = new HttpClient();
-        
-        // Key from jsb@cs.rit.edu
-        //{"email":"jsb@cs.rit.edu","key":"AAAAAwEAAQAAAQB7w4yJG+kH5BXhL9lgeCxkNqKeIIyC0zzG0FYJu5/WVa7xCdXGSmG3pEEpyEPhe
-        //81L9zb1qWpnn9yoiMPPawtDoZ26Um0LA/MAx/n4UdBENyWYd807+ex1h/uJ/GHgeZI/8yZ5LapCTNXaAwXvTfSY4OTG9hEgTJ6uK7cM11hn/qK
-        //07EnH1beaGoj/FOATFPqpLkDaz/fOkRQIQr6F41ks0PIJXjzmMeIJdUhBsluJaU/pllHqjTDFk2uBOSQr5g0WFeCVLfss0EYbkbx3BsLtvThDg
-        //phBc98KOU2gx3o+Tm5U1oTT/tZdUjrWq8iPWzI+JMrG1RtZEVVeewOFT5sn"}
-
-        static void Main(string[] args)
-        {
-            
-        }
-
-        static void keyGen(int bitsize)
-        {
-            /// Change to implelment email input or put in Main
-            var p = bitsize / 2;
-            var q = bitsize - p;
-            var N = p * q;
-            var r = (p - 1) * (q - 1);
-            var E = GenerateRandomInt(65536);
-            var D = modInverse(E, r);
-            var encodedPublicKey = Convert.ToBase64String(E.ToByteArray());
-            var encodedPrivateKey = Convert.ToBase64String(D.ToByteArray());
-            var publicKeyFile = new EncryptedFile();
-        }
-
-        static void sendKey(String email)
-        {
-            
-        }
-
-        static async void getKey(String email)
-        {
-            var response = await client.GetAsync("http://kayrun.cs.rit.edu:5000/Key/jsb@cs.rit.edu");
-            response.EnsureSuccessStatusCode();
-            var responseBody = await response.Content.ReadAsStringAsync();
-        }
-
-        static void sendMsg(String email, String text)
-        {
-            
-        }
-
-        static async void getMsg(String email)
-        {
-            
-            
-        }
-
-        static BigInteger modInverse(BigInteger a, BigInteger n)
-        {
-            BigInteger i = n, v = 0, d = 1;
-            while (a > 0)
-            {
-                BigInteger t = i / a, x = a;
-                a = i % x;
-                i = x;
-                x = d;
-                d = v - t * x;
-                v = x;
-            }
-
-            v %= n;
-            if (v < 0) v = (v + n) % n;
-            return v;
-        }
-        
         /// <summary>
         /// Method in charge of generating and checking a BigInteger based on the bit inputted.
         /// Loop is started if the generated integer is not a valid Prime number checked by IsProbablePrime.
